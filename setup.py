2f41b076b28d1128a5d5a5245448c369916d2a3e
Temchenko Alexander <ksanderer@gmail.com>
2017-11-15 12:09:55 +0300

    entry point changed to bitq

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 import os, errno
 
+DEFAULT_CONFIG_DIR = '/etc/bitq/'
+
 setup_options = dict(
-    name='bp',
+    name='bitq',
     version='0.0.1',
-    description='Backup cli tool.',
-    url='http://github.com/storborg/funniest',
+    description='bitqwe.com CLI tool',
+    url='https://github.com/ksanderer/bitq-cli',
     author='Alex Temchenko',
     author_email='ksanderer@gmail.com',
     license='MIT',
@@ -17,23 +19,29 @@ setup_options = dict(
     install_requires=['configparser', 'requests', 'click', 'peewee', 'bravado'],
     entry_points={
         'console_scripts': [
-              'bp = bp.main:cli'
+              'bitq = bp.main:cli'
           ]
     },
     data_files=[
-        ('/etc/bp/', ['bp/config/main.conf']),
-        ('/etc/bp/projects', ['bp/config/projects/project_name.conf']),
+        (DEFAULT_CONFIG_DIR, ['bp/config/main.conf']),
+        (DEFAULT_CONFIG_DIR + 'projects', ['bp/config/projects/project_name.conf']),
     ],
     zip_safe=True
 )
 
 try:
-    os.mkdir('/etc/bp')
+    try:
+        os.mkdir(DEFAULT_CONFIG_DIR)
+    except OSError as e:
+        if type(e).__name__ == "FileExistsError":
+            pass
+        else:
+            raise e
+
     setup(**setup_options)
 except OSError as e:
     if type(e).__name__ == "FileExistsError":
         setup(**setup_options)
-        exit()
 
 
     if type(e).__name__ == "PermissionError" or \
@@ -48,7 +56,7 @@ except OSError as e:
             "\r\n"
             "Warning!\r\n"
             "\r\n"
-            "Can't write to /etc/bp (default config directory)\r\n"
+            "Can't write to " + DEFAULT_CONFIG_DIR + " (default config directory)\r\n"
             "In order to use default config dir /etc/bp you need to manually create it\r\n"
             "take a look at:\r\n"
             "\r\n"
